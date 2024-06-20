d06a72ee75c7e8349205b50e29cc34a9633a1e11
tpneik <tpneik@gmail.com>
2024-06-20 16:26:42 +0000

    retive script while login

--- CLIENT/controller/login.py
+++ CLIENT/controller/login.py
@@ -18,5 +18,21 @@ def login_client():
     res = requests.post(f"http://{ip}:5000/login", json=cred)
     print(f"[6] response returned!")
     res = res.json()
+    try:
+        # This gonna change to script file because it gonna retrive script.sh for installation
+        response = requests.get(f"http://{ip}:5000/zip_file", headers={
+            'Authorization': str(res["data"]['token'])
+        })
+        response.raise_for_status()
+        if response.headers['content-type'] == 'application/zip':
+            filename = 'CLIENT/resource/flower-homomorphic_encryption.zip'
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+        
+            print(f"ZIP file saved as: {filename}")
+        else:
+            print("Response content is not a ZIP file.")
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
     save_data(res)
     return {"status": "Succesfully"}
