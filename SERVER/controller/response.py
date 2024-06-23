25d878654034e89bbb8013d53ed8735d0fe54070
tpneik <tpneik@gmail.com>
2024-06-23 15:13:04 +0000

    audit the ressponse.py and resource

--- SERVER/controller/response.py
+++ SERVER/controller/response.py
@@ -15,25 +15,6 @@ def zip_file(_user):
     return send_file(FILEPATH, 
                      mimetype='application/zip'
                     )
-    # fin = open(FILEPATH, 'r')
-    # result = send_file(fin, as_attachment=True)
-    # print(result)
-    # return result
-####
-    # FILEPATH = f'/home/ubuntu/kltn/SERVER/resources/zip_file/flower-homomorphic_encryption.zip'
-    # fileobj = io.BytesIO()
-    # with zipfile.ZipFile(fileobj, 'w') as zip_file:
-    #     zip_info = zipfile.ZipInfo(FILEPATH)
-    #     zip_info.date_time = time.localtime(time.time())[:6]
-    #     zip_info.compress_type = zipfile.ZIP_DEFLATED
-    #     with open(FILEPATH, 'rb') as fd:
-    #         zip_file.writestr(zip_info, fd.read())
-    # fileobj.seek(0)
-
-    # # Changed line below
-    # return Response(fileobj.getvalue(),
-    #                 mimetype='application/zip',
-    #                 headers={'Content-Disposition': 'attachment;filename=your_filename.zip'})
     
     
 @token_required
